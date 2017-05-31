4c49e94f5e1913f574d1772330a6c4910ad811ca
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-05-31 14:34:39 -0600

    Specify adding multiple items to a cart

--- spec/features/cart/user_adds_item_to_cart_spec.rb
+++ spec/features/cart/user_adds_item_to_cart_spec.rb
@@ -2,13 +2,16 @@ require 'rails_helper'
 
 feature "Cart" do
   scenario "visitor adds an item to their cart" do
-    item = create(:item)
+    item = create(:item, title: "Bespoke Beard Oil Brush")
 
     visit items_path
     within(".item_#{item.id}") do
       click_button "Add to Cart"
     end
-    expect(page).to have_content("You have added 1 #{item.title} to your cart.")
+    within(".item_#{item.id}") do
+      click_button "Add to Cart"
+    end
+    expect(page).to have_content("Your cart now contains 2 Bespoke Beard Oil Brushes.")
     expect(page).to have_current_path(items_path)
   end
 end
