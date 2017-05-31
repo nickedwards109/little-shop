505196e2d740ee82289e780e8870bcd6d6836b65
Alexander Koch <alex549us3@gmail.com>
2017-05-31 13:38:00 -0600

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(version: 20170531002820) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
