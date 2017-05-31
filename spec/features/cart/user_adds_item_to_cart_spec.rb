413aea3f90354ce2729a7433e38a02af515df95e
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-05-31 16:00:00 -0600

    Specify total item count in cart

--- spec/features/cart/user_adds_item_to_cart_spec.rb
+++ spec/features/cart/user_adds_item_to_cart_spec.rb
@@ -1,17 +1,23 @@
 require 'rails_helper'
 
 feature "Cart" do
-  scenario "visitor adds an item to their cart" do
+  scenario "visitor adds items to their cart" do
     item = create(:item, title: "Bespoke Beard Oil Brush")
 
     visit items_path
     within(".item_#{item.id}") do
       click_button "Add to Cart"
     end
+
+    expect(page).to have_current_path(items_path)
+    expect(page).to have_content("Your cart now contains 1 Bespoke Beard Oil Brush.")
+    expect(page).to have_content("Cart (1)")
+
     within(".item_#{item.id}") do
       click_button "Add to Cart"
     end
+    
     expect(page).to have_content("Your cart now contains 2 Bespoke Beard Oil Brushes.")
-    expect(page).to have_current_path(items_path)
+    expect(page).to have_content("Cart (2)")
   end
 end
