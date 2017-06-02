a9f3df0eded6a21b1ef4f2493780287cedaeeaf9
rongxanh88 <rongxanh88@gmail.com>
2017-06-02 11:55:39 -0600

    refactoring user can edit spec

--- spec/features/users/user_can_edit_account_spec.rb
+++ spec/features/users/user_can_edit_account_spec.rb
@@ -3,12 +3,13 @@ require 'rails_helper'
 RSpec.feature 'User', type: :feature do
   scenario 'can edit own account' do
     user = create(:user)
-    visit(login_path)
-    fill_in 'user[username]', with: user.username
-    fill_in 'user[password]', with: user.password
-    click_on('Submit Login')
-
-    expect(page).to have_current_path(dashboard_path)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    # visit(login_path)
+    # fill_in 'user[username]', with: user.username
+    # fill_in 'user[password]', with: user.password
+    # click_on('Submit Login')
+    visit(dashboard_path)
+    # expect(page).to have_current_path(dashboard_path)
     click_on('Edit Account')
     fill_in 'user[username]', with: 'gooduser'
     fill_in 'user[password]', with: 'new_password'
