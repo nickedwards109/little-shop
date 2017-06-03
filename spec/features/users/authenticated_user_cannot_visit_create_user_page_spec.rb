538f58fee90b2014d4c4e349b1c486663abd53fb
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-06-03 13:37:46 -0600

    Add more authorization specs
    A user cannot visit the login page once already authenticated. A user cannot visit the create user page once already authenticated.

---
+++ spec/features/users/authenticated_user_cannot_visit_create_user_page_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.feature "User", type: :feature do
+  let(:user) {create(:user)}
+
+  scenario "cannot visit the create user page once already authenticated" do
+    visit(login_path)
+    fill_in "user[username]", with: user.username
+    fill_in "user[password]", with: user.password
+    click_on('Submit Login')
+
+    visit new_user_path
+    expect(page).to have_current_path(root_path)
+end
---
+++ spec/features/users/authenticated_user_cannot_visit_login_page_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.feature "User", type: :feature do
+  let(:user) {create(:user)}
+
+  scenario "cannot visit the login page once already authenticated" do
+    visit(login_path)
+    fill_in "user[username]", with: user.username
+    fill_in "user[password]", with: user.password
+    click_on('Submit Login')
+
+    visit login_path
+    expect(page).to have_current_path(root_path)
+end
