2594ad010523df87d898bbb634a49f97df148edd
rongxanh88 <rongxanh88@gmail.com>
2017-06-03 09:04:56 -0600

    refactor user controller done

--- app/controllers/users/dashboard_controller.rb
+++ app/controllers/users/dashboard_controller.rb
@@ -1,5 +1,4 @@
 class Users::DashboardController < AuthenticateUserController
-  
   def home
     @user = User.find(session[:user_id])
   end
--- spec/features/users/user_can_login_spec.rb
+++ spec/features/users/user_can_login_spec.rb
@@ -24,4 +24,16 @@ RSpec.feature "User", type: :feature do
     expect(page).to have_current_path(login_path)
     expect(page).to have_content('Login Failed')
   end
+
+  scenario 'logs in with uppercase username' do
+    visit(login_path)
+    fill_in "user[username]", with: user.username.upcase
+    fill_in "user[password]", with: user.password
+    click_on('Submit Login')
+
+    expect(page).to have_current_path('/dashboard')
+    expect(page).to have_content("Logged in as #{user.username}")
+    expect(page).not_to have_content("Login")
+    expect(page).to have_content("Logout")
+  end
 end
