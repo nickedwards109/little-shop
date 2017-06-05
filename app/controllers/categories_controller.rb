5dd484d2e0b9b27bc3bcdbeb8e2f374b852a0578
rongxanh88 <rongxanh88@gmail.com>
2017-06-05 16:38:27 -0600

    created search tools in navbar

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,14 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    @category = Category.find(params[:id])
+    if params[:search]
+      @category = Category.search(params[:search])
+      if @category.nil?
+        redirect_to items_path, notice: 'Unable to find Category'
+      end
+    else
+      @category = Category.find(params[:id])
+    end
     @items = @category.items.paginate(:page => params[:page], :per_page => 16)
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,8 @@ class Category < ApplicationRecord
   has_many :items
 
   validates :title, presence: true, uniqueness: true
+
+  def search(search)
+    where("title LIKE ?", "%#{search}%")
+  end
 end
