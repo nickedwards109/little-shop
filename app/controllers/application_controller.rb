6d70fe5e79ee3ddcc760331289e9bdddfc069adb
Alex Koch <alex549us3@gmail.com>
2017-06-30 11:45:00 -0600

    autocomplete search actually working 🎉✨

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :set_cart, :set_categories
+  before_action :set_cart, :set_categories, :set_search
   helper_method :current_user, :current_admin?
 
   def set_cart
@@ -9,6 +9,10 @@ class ApplicationController < ActionController::Base
 
   def set_categories
     @categories = Category.all
+  end
+
+  def set_search
+    @items_all = Item.all
   end
 
   def current_user
