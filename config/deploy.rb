7a1903ffb4c9269442190db210feaa7fb29a699e
Alex Koch <alex549us3@gmail.com>
2017-07-24 11:19:24 -0600

    holy shit it fucking works!!

--- config/deploy.rb
+++ config/deploy.rb
@@ -64,16 +64,6 @@ namespace :puma do
 end
 
 namespace :deploy do
-  desc "Make sure local git is in sync with remote."
-  task :check_revision do
-    on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
-        puts "Run `git push` to sync changes."
-        exit
-      end
-    end
-  end
 
   desc 'Initial Deploy'
   task :initial do
@@ -90,7 +80,11 @@ namespace :deploy do
     end
   end
 
-  before :starting,     :check_revision
+  desc "reload the database with seed data"
+  task :seed do
+    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=production"
+  end
+
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
   after  :finishing,    :restart
